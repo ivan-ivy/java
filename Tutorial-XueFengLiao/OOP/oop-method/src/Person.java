37b314a7038f30f18094eed11f9b85704b375f26
ivan <yl4314@columbia.edu>
2019-11-11 10:13:41 -0500

    OOP first 3 parts

---
+++ Tutorial-XueFengLiao/OOP/oop-method/src/Person.java
@@ -0,0 +1,27 @@
+
+/**
+ * Learn Java from https://www.liaoxuefeng.com/
+ * 
+ * @author liaoxuefeng
+ */
+public class Person {
+
+	private String name;
+	private double age;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getAge(){
+		return this.age;
+	}
+
+	public void setAge(double age){
+		this.age=age;
+	}
+}
